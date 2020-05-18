345c417ababf66f0133c2ceb76fd292ca2252dd9
David Nemec <d4v1d.nemec@gmail.com>
2020-05-18 15:51:35 +0200

    Add comments to PPM model

--- Paq1.Core/Models/ContextComparer.cs
+++ Paq1.Core/Models/ContextComparer.cs
@@ -4,27 +4,35 @@ namespace Paq1.Core.Models
 {
     public class ContextComparer : IEqualityComparer<int[]>
     {
-        public bool Equals(int[] a, int[] b)
+        public bool Equals(int[] x, int[] y)
         {
-            for (int i = 0; i < a.Length; i++)
+            if (x == null || y == null)
+                return false;
+
+            if (x.Length != y.Length)
+                return false;
+
+            for (int i = 0; i < x.Length; i++)
             {
-                if (a[i] != b[i])
-                {
+                if (x[i] != y[i])
                     return false;
-                }
             }
+
             return true;
         }
-        public int GetHashCode(int[] a)
+
+        // based on https://stackoverflow.com/questions/14663168/an-integer-array-as-a-key-for-dictionary
+        public int GetHashCode(int[] obj)
         {
             int hash = 17;
-            for (int i = 0; i < a.Length; i++)
+            foreach (var i in obj)
             {
                 unchecked
                 {
-                    hash = hash * 23 + a[i];
+                    hash = hash * 23 + i;
                 }
             }
+
             return hash;
         }
     }
--- Paq1.Core/Models/PartialNonStationaryPpmModel.cs
+++ Paq1.Core/Models/PartialNonStationaryPpmModel.cs
@@ -1,25 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Paq1.Core.Models
 {
     public class PartialNonStationaryPpmModel : IModel
     {
         public int Order { get; }
-        private readonly int[] _context;
+        private int[] _context;
         private readonly Dictionary<int[], int[]> _counters = new Dictionary<int[], int[]>(new ContextComparer());
 
         public PartialNonStationaryPpmModel(int order)
         {
             Order = order;
-            _context = new int[order + 1];
-            _context[order] = 1;
+            _context = new int[order + 1]; // create context bytes and one partial byte
+            _context[order] = 1; // first bit serves as a mark of byte completeness
         }
 
         public (int, int) Predict(int n0, int n1)
         {
             if (!_counters.ContainsKey(_context))
             {
-                _counters[_context.Clone() as int[]] = new int[2];
+                _counters[_context] = new int[2];
             }
 
             var c = _counters[_context];
@@ -38,10 +39,16 @@ namespace Paq1.Core.Models
                 el[b] = el[b] / 2 + 1;
             }
 
-            _context[Order] = (_context[Order] << 1) | bit;
-            if (_context[Order] >= 256)
+            UpdateContext(bit);
+        }
+
+        private void UpdateContext(int bit)
+        {
+            _context = (int[]) _context.Clone(); // clone, because we don't want to mutate context, which is used as key in dictionary
+            _context[Order] = (_context[Order] << 1) | bit; // add bit to context of partial byte
+            if (_context[Order] >= 256) // if byte is completed, move it to the following context
             {
-                _context[Order] = _context[Order] & 0xff;
+                _context[Order] = _context[Order] & 0xff; // remove bit used as a byte mark
                 for (int i = 0; i < Order; i++)
                 {
                     _context[i] = _context[i + 1];
